17a8e814555870c87d926aad7901e1acd21e0367
TLC1 <TLC1@10.0.20.24>
2018-11-22 15:23:48 +0530

    code change

--- src/main/java/com/ms/calc/Calculator.java
+++ src/main/java/com/ms/calc/Calculator.java
@@ -3,15 +3,11 @@ package com.ms.calc;
 public class Calculator{
 	
 	public double Add(double a,double b){
-		return a+b;
-	}
+		return a+b;	}
 	public double Sub(double a,double b){
-		return a-b;
-	}
+		return a-b;	}
 	public double Mul(double a,double b){
-		return a*b;
-	}
+		return a*b;	}
 	public double Div(double a,double b){
-		return a/b;
-	}
+		return a/b;	}
 }
