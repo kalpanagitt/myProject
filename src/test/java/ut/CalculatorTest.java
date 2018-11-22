0445422f030f0325f955564447e8a65edd922ed1
TLC1 <TLC1@10.0.20.24>
2018-11-22 16:36:26 +0530

    code

---
+++ src/test/java/ut/CalculatorTest.java
@@ -0,0 +1,37 @@
+package ut;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import com.ms.calc.*;
+
+public class CalculatorTest{
+	
+	static Calculator cobj;
+	
+	@BeforeClass
+	public static void setUp(){
+		cobj=new Calculator();
+	}
+	
+	@AfterClass
+	public static void tearDown(){
+		System.gc();
+	}
+	@Test
+	public void testAdd(){
+		assertTrue(cobj.Add(3, 3)!=0);
+	}
+	@Test
+	public void testSub(){
+		assertTrue(cobj.Sub(3, 3)==0);
+	}
+	@Test
+	public void testMul(){
+		assertTrue(cobj.Mul(3, 3)==9);
+	}
+	@Test
+	public void testDiv(){
+		assertTrue(cobj.Div(3, 3)!=0);
+	}
+}
